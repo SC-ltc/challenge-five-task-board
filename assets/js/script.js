67fca3e1de97372ceb5540818bff580ddf13e278
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-10-28 10:55:33 -0700

    added code to the to-dos for creating a task card, rending the task list, and adding a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,35 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+// Grab references to the important DOM elements.
+const taskDisplayEl = $('#task-display');
+const taskFormEl = $('#task-form');
+const taskNameInputEl = $('#task-name-input');
+const taskDescInputEl = $('#task-desc-input');
+const taskDateInputEl = $('#taskDueDate');
+const modalForm = $('modal-form');
+
+// Reads tasks from local storage and returns array of task objects.
+// If there are no tasks in localStorage, it initializes an empty array ([]) and returns it.
+function readTasksFromStorage() {
+  // Retrieve tasks from localStorage and parse the JSON to an array.
+  // We use `let` here because there is a chance that there are no tasks in localStorage (which means the tasks variable will be equal to `null`) and we will need it to be initialized to an empty array.
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  // If no tasks were retrieved from localStorage, assign tasks to a new empty array to push to later.
+  if (!tasks) {
+    tasks = [];
+  }
+
+  // Return the tasks array either empty or with data in it whichever it was determined to be by the logic right above.
+  return tasks;
+}
+
+// Accepts an array of tasks, stringifys them, and saves them in localStorage.
+function saveTasksToStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -9,17 +38,119 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+    .addClass('card project-card draggable my-3')
+    .attr('data-project-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-project-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
+
+  // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  return taskCard;
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+  const tasks = readTasksFromStorage();
+
+  // ? Empty existing project cards out of the lanes
+  const todoList = $('#todo-cards');
+  todoList.empty();
+
+  const inProgressList = $('#in-progress-cards');
+  inProgressList.empty();
+
+  const doneList = $('#done-cards');
+  doneList.empty();
+
+  // ? Loop through projects and create project cards for each status
+  for (let task of tasks) {
+    if (task.status === 'to-do') {
+      todoList.append(createTaskCard(task));
+    } else if (task.status === 'in-progress') {
+      inProgressList.append(createTaskCard(task));
+    } else if (task.status === 'done') {
+      doneList.append(createTaskCard(task));
+    }
+  }
+
+  // ? Use JQuery UI to make task cards draggable
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+    helper: function (e) {
+      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+  
+  event.preventDefault();
 
+  // Read user input from the modal form
+  const taskName = taskNameInputEl.trim();
+  const taskDesc = taskDescInputEl.val();
+  const taskDate = taskDateInputEl.val();
+
+  const newTask = {
+    id: generateTaskId(),
+    name: taskName,
+    description: taskDesc,
+    dueDate: taskDate,
+    status: 'to-do',
+  };
+
+  // Pull the tasks from localStorage and push the new project to the array
+  const tasks = readTasksFromStorage();
+  tasks.push(newTask);
+
+  // Save the updated tasks array to localStorage
+  saveTasksToStorage(tasks);
+
+  // Print task data back to the screen
+  renderTaskList();
+
+  // Clear the form inputs
+  taskNameInputEl.val('');
+  taskDescInputEl.val('');
+  taskDateInputEl.val('');
 }
 
 // Todo: create a function to handle deleting a task
